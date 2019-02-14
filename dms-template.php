0de2afabba81b789ee9d6ea2828642187bf7e35f
Dixon Martinez <dixonalvarezm@gmail.com>
2019-02-13 20:55:25 -0400

    Add file with description of this plugin template

---
+++ dms-template.php
@@ -0,0 +1,14 @@
+<?php
+/** File of DMS Template Plugin
+ *
+ * @package dms-template
+ */
+
+/*
+Plugin Name: DMS Template Plugin
+Plugin URI: https://github.com/dixonmartinez/dms-template
+Description: A simple and fully functional WordPress plugin template that does NOTHING and provides an excellent advantage in building a new quality complement.
+Author: Dixon Martinez Systems
+Version: 0.0.1
+Author URI: https://dixonmartinez.github.io
+ */
